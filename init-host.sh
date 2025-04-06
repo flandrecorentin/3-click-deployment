f64a5e6cd0f74c7c5d047caa87d3bd13ce65947b
root <flandrecorentin@gmail.com>
2025-04-06 18:34:44 +0000

    Add a script to initialize a project on 3-click-deployment

--- init-host.sh
+++ init-host.sh
@@ -8,21 +8,28 @@ debug="true"
 
 # Argument treatment (validation and initialization)
 if [ "$#" -eq 2 ]; then
-    echo "[INFO] Executing init-host.sh with $0 <argument> id: $1 <argument>"
-    config_file="$0"
-    id="$1"
+    echo "[INFO] Executing init-host.sh with |$1| id: |$2|"
+    config_file="$1"
+    id="$2"
 elif [ "$#" -eq 1 ]; then
-  echo "[INFO] Executing init-host.sh with  config.json id: $0 <argument>"
+  echo "[INFO] Executing init-host.sh with config.json id: |$1|"
   config_file="config.json"
-  id="$0"
+  id="$1"
 else
   echo "[ERROR] Incorrect number of argument"
   exit 1
 fi
 
 # Use jq to filter and retrieve the block with id "portfolio"
-json_block=$(jq '.[] | select(.id == "$id")' "$json_file")
-if [ "$debug" = "true" ]; then echo "[DEBUG] json_block: |$path_repositories|"; fi
+json_block=$(jq --arg id "$id" '.[] | select(.id == $id)' "$config_file")
+# Check if the string is empty
+if [ -z "$json_block" ]; then
+  echo "[ERROR] No json block with id |$id| into |$config_file|"; exit 1;
+fi
+
+if [ "$debug" = "true" ]; then echo "[DEBUG] json_block: |$json_block|"; fi
+port=$(echo $json_block| jq -r '.port_1')
+dns=$(echo $json_block| jq -r '.dns')
 
 # Validate json
 
@@ -30,8 +37,30 @@ if [ "$debug" = "true" ]; then echo "[DEBUG] json_block: |$path_repositories|"; 
 # init nginx config (with dns and host)
 
 # Nginx
-cat "$path_3_click_deployment_default_nginx_conf" > "$path_nginx_sites/$id"
-# TODO Replace nginx values
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[INFO] ---------------------$now--------------------------"
+path_nginx_file="$path_nginx_sites/$id"
+cat "$path_3_click_deployment_default_nginx_conf" > "$path_nginx_file"
+echo "[INFO] Create nginx file |$path_nginx_file| using template |$path_3_click_deployment_default_nginx_conf|"
+sed -i "s|<DNS>|$dns|g" "$path_nginx_file"
+echo "[INFO] Edit <DNS> with |$dns|"
+sed -i "s|<PORT>|$port|g" "$path_nginx_file"
+echo "[INFO] Edit <PORT> with |$port|"
+echo "[INFO] -----------------------------------------------------"; echo ""
+
+# Restart nginx service
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[INFO] ---------------------$now--------------------------"
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[INFO] Restarting nginx start at $now"
+sudo systemctl restart nginx
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[INFO] Restarting nginx finished at $now"
+echo "[INFO] -----------------------------------------------------"; echo ""
 
 # Certbot
-sudo certbot certonly --nginx --non-interactive --agree-tos -d "$dns"
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[INFO] ---------------------$now--------------------------"
+echo "[INFO] Run certbot for ngin for following dns: |$dns|"
+sudo certbot --nginx --non-interactive --agree-tos -d "$dns"
+echo "[INFO] -----------------------------------------------------"; echo ""
