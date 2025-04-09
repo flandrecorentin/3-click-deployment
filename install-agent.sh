b17a4c219291b4367c1460921897ef08c650eff2
root <flandrecorentin@gmail.com>
2025-04-09 17:18:13 +0000

    Add script to install 3-click-deployment agent

---
+++ install-agent.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update
+
+# Install jq
+sudo apt install jq
+
+# Install and start nginx
+sudo apt install nginx
+sudo service nginx restart
+
+# Install certbot
+sudo apt install snapd
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+# Install github cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+sudo touch .github
+echo 'token=' > .github
+# YOU NEED TO ADD YOUR GITHUB TOKEN IN THE /3-click-deployment/.github FILE
+# GO TO https://github.com/settings/tokens TO CREATE YOUR TOKEN
+
+base_logs='/3-click-deployment/logs/'
+base_repositories='/3-click-deployment/repositories/'
+sudo mkdir "$base_logs"
+sudo mkdir "$base_repositories"
+sudo touch config.json
